6d33fc245690857382587797d661e885dd6b9816
Olena Pletnova <letneva2701@gmail.com>
2025-10-21 21:04:03 +0300

    + component editBoardName

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -1,6 +1,7 @@
-import { JSX, useState, useEffect } from 'react';
+import { JSX, useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import instance from '../../api/request';
+import { EditBoardName } from './components/EditBoardName';
 import boardStyle from './components/Board1/board.module.scss';
 import { List } from './components/List';
 import { IList } from '../../common/interfaces/IList';
@@ -9,20 +10,22 @@ export function Board(): JSX.Element {
   const [title, setTitle] = useState<string>('');
   const [lists, setLists] = useState<IList[]>([]);
   const [errorText, setErrorText] = useState<string>('');
+  const [inputName, setInputName] = useState(false);
   const { boardId } = useParams();
 
+  const fetchData = async (): Promise<void> => {
+    try {
+      const { data } = await instance.get(`/board/${boardId}`);
+      setLists(data.lists);
+      setTitle(data.title);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      setErrorText('Помилка при завантаженні даних');
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async (): Promise<void> => {
-      try {
-        const { data } = await instance.get(`/board/${boardId}`);
-        setLists(data.lists);
-        setTitle(data.title);
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(error);
-        setErrorText('Помилка при завантаженні даних');
-      }
-    };
     fetchData();
   }, []);
 
@@ -41,15 +44,25 @@ export function Board(): JSX.Element {
   return (
     <div className={boardStyle.board}>
       <div className={boardStyle.board__header}>
-        <h1 className={boardStyle.board__textHeader}>
-          {title} {boardId}
-        </h1>
+        {inputName ? (
+          <EditBoardName
+            defaultValue={title}
+            onCardCreated={fetchData}
+            idBoard={boardId}
+            setInput={setInputName}
+            setInputError={setErrorText}
+          />
+        ) : (
+          <h1 className={boardStyle.board__textHeader} onClick={(): void => setInputName(true)}>
+            {title}
+          </h1>
+        )}
         <button className={boardStyle.board__deleteButton} onClick={deleteBoard}>
           Видалити дошку
         </button>
       </div>
       {errorText && (
-        <h2 className={boardStyle.board__textHeader} style={{ color: 'lime' }}>
+        <h2 className={boardStyle.board__textHeader} style={{ color: 'brown' }}>
           {errorText}
         </h2>
       )}
---
+++ src/pages/Board/components/EditBoardName.tsx
@@ -0,0 +1,58 @@
+import { ChangeEvent, JSX, useState } from 'react';
+import instance from '../../../api/request';
+import editBoardName from './editBoardName.module.scss';
+import boardStyle from './Board1/board.module.scss';
+
+interface props {
+  defaultValue: string;
+  onCardCreated: () => Promise<void>;
+  idBoard: string | undefined;
+  setInput: React.Dispatch<React.SetStateAction<boolean>>;
+  setInputError: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export function EditBoardName({ defaultValue, onCardCreated, idBoard, setInput, setInputError }: props): JSX.Element {
+  const [inputValue, setInputValue] = useState(defaultValue);
+  const [errorInput, setErrorInput] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (/^[a-zA-Zа-щА-ЩіІїЇєЄґҐ0-9 ,._-]*$/.test(event.target.value)) {
+      setInputValue(event.target.value);
+    }
+  };
+
+  const editName = async (): Promise<void> => {
+    if (inputValue.trim() === '') {
+      setInputError('Ім`я дошки не повинно бути пустим.');
+      return;
+    }
+    try {
+      await instance.put(`/board/${idBoard}`, { title: inputValue });
+      onCardCreated();
+      setInput(false);
+      setInputError('');
+    } catch (error) {
+      setInput(false);
+      setInputError('Помилка при спробі зміни назви дошки.');
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (event.key === 'Enter') {
+      editName();
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      value={inputValue}
+      onChange={handleChange}
+      className={editBoardName.input}
+      onBlur={editName}
+      onKeyDown={handleKeyDown}
+      // eslint-disable-next-line jsx-a11y/no-autofocus
+      autoFocus
+    />
+  );
+}
