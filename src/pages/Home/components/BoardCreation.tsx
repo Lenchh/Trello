3422c6b871c6413fbbd084e3d5a72891dcece0de
Olena Pletnova <letneva2701@gmail.com>
2026-07-03 22:20:04 +0300

    update design of modal window

--- src/pages/Home/components/BoardCreation.tsx
+++ src/pages/Home/components/BoardCreation.tsx
@@ -1,8 +1,12 @@
-import { ChangeEvent, Dispatch, JSX, SetStateAction, useRef, useState } from 'react';
+import { ChangeEvent, Dispatch, JSX, MouseEventHandler, SetStateAction, useRef, useState } from 'react';
 import boardCreateStyle from './boardCreation.module.scss';
 import { toastrInfo } from '../../../common/toastr/info/toastr-options-info';
 import { useAppDispatch } from '../../../featchers/hooks';
 import { createBoard } from '../../../featchers/slices/boardSlice';
+import colorButton from '../../../assets/colorButton.svg';
+import colorButtonActive from '../../../assets/colorButtonActive.svg';
+import imageButton from '../../../assets/imageButton.svg';
+import imageButtonActive from '../../../assets/imageButtonActive.svg';
 
 interface props {
   onRefresh: () => Promise<void>;
@@ -12,10 +16,11 @@ interface props {
 export function BoardCreation({ onRefresh, openCreationForm }: props): JSX.Element {
   const dispatch = useAppDispatch();
   const [inputValue, setInputValue] = useState<string>('');
-  const [inputBackground, setInputBackground] = useState<string>('#136CF1');
+  const [inputBackground, setInputBackground] = useState<string>('');
   const [selectedOption, setSelectedOption] = useState('color');
   const imageBackgroundRef = useRef<HTMLInputElement>(null);
   const [nameFile, setNameFile] = useState('');
+  const pastelBackgrounds = ['#568b9e', '#cb938f', '#bede8b', '#fbd072', '#C3B1E1', '#8bb8a7', '#ed7085', '#337849'];
 
   const closeDialog = (): void => {
     if (imageBackgroundRef.current) imageBackgroundRef.current.value = '';
@@ -37,6 +42,10 @@ export function BoardCreation({ onRefresh, openCreationForm }: props): JSX.Eleme
       toastrInfo("Ім'я дошки не повинно бути пустим", 'Інформація');
       return;
     }
+    if (inputBackground.trim() === '') {
+      toastrInfo('Оберіть колір або зображення фону дошки', 'Інформація');
+      return;
+    }
     try {
       await dispatch(createBoard({ inputValue, inputBackground })).unwrap();
       onRefresh();
@@ -52,12 +61,12 @@ export function BoardCreation({ onRefresh, openCreationForm }: props): JSX.Eleme
     }
   };
 
-  const changeOption = (event: ChangeEvent<HTMLInputElement>): void => {
-    setSelectedOption(event.target.value);
+  const handleInputColor = (event: ChangeEvent<HTMLInputElement>): void => {
+    setInputBackground(event.target.value);
   };
 
-  const handleColor = (event: ChangeEvent<HTMLInputElement>): void => {
-    setInputBackground(event.target.value);
+  const handleColor = (color: string): void => {
+    setInputBackground(color);
   };
 
   const handleImage = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -77,59 +86,78 @@ export function BoardCreation({ onRefresh, openCreationForm }: props): JSX.Eleme
       <div className={boardCreateStyle.dialogWindow}>
         <h2 className={boardCreateStyle.header}>Створення дошки</h2>
         <form onSubmit={createBoardData}>
-          <label className={boardCreateStyle.form}>
-            Ім'я дошки:
-            <input type="text" value={inputValue} onChange={handleChange} className={boardCreateStyle.input} />
+          <h3>Ім'я дошки:</h3>
+          <label className={boardCreateStyle.formNameBoard}>
+            <input type="text" value={inputValue} onChange={handleChange} placeholder="Введіть назву дошки..." />
           </label>
-          <div className={boardCreateStyle.form}>
-            Колір Фону:
-            <br />
-            <label>
-              <input
-                type="radio"
-                name="boardAction"
-                value="color"
-                checked={selectedOption === 'color'}
-                onChange={changeOption}
-              />
-              Колір
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="boardAction"
-                value="image"
-                checked={selectedOption === 'image'}
-                onChange={changeOption}
-              />
-              Зображення
-            </label>
-            <br />
-            {selectedOption === 'color' ? (
+          <h3>Колір або Зображення Фону:</h3>
+          <div className={boardCreateStyle.customControl}>
+            <button
+              className={selectedOption === 'color' ? boardCreateStyle.active : ''}
+              onClick={(e): void => {
+                e.preventDefault();
+                e.stopPropagation();
+                setSelectedOption('color');
+              }}
+            >
+              <img src={selectedOption === 'color' ? colorButtonActive : colorButton} alt="color icon" />
+              <span>Колір</span>
+            </button>
+            <button
+              className={selectedOption === 'image' ? boardCreateStyle.active : ''}
+              onClick={(e): void => {
+                e.preventDefault();
+                e.stopPropagation();
+                setSelectedOption('image');
+              }}
+            >
+              <img src={selectedOption === 'image' ? imageButtonActive : imageButton} alt="" />
+              <span>Зображення</span>
+            </button>
+          </div>
+          {selectedOption === 'color' ? (
+            <div className={boardCreateStyle.colorsBackground}>
+              {' '}
+              {pastelBackgrounds.map((color) => (
+                <button
+                  key={color}
+                  style={{ backgroundColor: color }}
+                  aria-label={`Вибрати колір фону ${color}`}
+                  onClick={(e): void => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    handleColor(color);
+                  }}
+                  className={`${boardCreateStyle.colorButton} ${inputBackground === color ? boardCreateStyle.activeColorButton : ''}`}
+                />
+              ))}
               <input
                 key="color"
                 type="color"
                 value={inputBackground}
-                onChange={handleColor}
-                className={boardCreateStyle.input}
+                onChange={handleInputColor}
+                className={boardCreateStyle.colorInputButton}
               />
-            ) : (
-              <>
-                <input
-                  key="image"
-                  ref={imageBackgroundRef}
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImage}
-                  className={boardCreateStyle.customFileInput}
-                />
-                {nameFile && <img src={nameFile} alt="" className={boardCreateStyle.previewBackground} />}
-              </>
-            )}
-          </div>
+            </div>
+          ) : (
+            <input
+              key="image"
+              style={{
+                backgroundImage: inputBackground ? `url(${inputBackground})` : `url(${imageButton})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+              }}
+              ref={imageBackgroundRef}
+              type="file"
+              accept="image/*"
+              onChange={handleImage}
+              className={boardCreateStyle.customFileInput}
+            />
+          )}
           <div className={boardCreateStyle.dialogActions}>
             <button type="submit" className={boardCreateStyle.action}>
-              Надіслати
+              Створити дошку
             </button>
             <button type="button" onClick={closeDialog} className={boardCreateStyle.action}>
               Закрити
